293d51ffb724d55da5d23e1c72f5a35dc49efb6c
Alexander Teliatnikov <labriko453@gmail.com>
2025-01-26 17:25:51 +0000

    feat: Add HttpErrorFilter

--- 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
+++ 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
@@ -1,5 +1,22 @@
-import { ArgumentsHost, ExceptionFilter } from "@nestjs/common";
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+import { Response } from 'express';
+import fs from 'fs';
 
+@Catch(HttpException)
 export class HttpErrorFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {}
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+
+    fs.appendFileSync('errors.log', `[${new Date()}] ${status} - Invalid ID`);
+
+    response
+      .status(status)
+      .json({
+        statusCode: status,
+        message: exception.message,
+        timestamp: new Date().toISOString(),
+      });
+  }
 }
